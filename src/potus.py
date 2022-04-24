31cc59aa7cda415017b13b952ca94175703813df
Bernhard Schiffer <schiffer.be@gmail.com>
2022-04-24 20:08:09 +0200

    solution

--- src/potus.py
+++ src/potus.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
-
+# %%
 # we will be using nltk.lm
-import nltk
+from nltk.lm import MLE
+from nltk.lm.preprocessing import padded_everygram_pipeline
+import pandas as pd
+import re
+import math
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.preprocessing import LabelEncoder
 
-
+# %%
 # 0. Before you get started, make sure to download the Obama and Trump twitter
 #    archives.
 
@@ -11,20 +18,203 @@ import nltk
 # tokenization method that strips unnecessary tokens but retains special
 # words such as mentions (@...) and hashtags (#...).
 
+def tokenize(sentence: str) -> list:
+    sentence = sentence.lower()
+
+    # replace urls
+    sentence = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '<url>', sentence)
+    
+    # replace special chars with spaces
+    for ch in [',','.','!','?','-','-','_','\s',':','"']:
+        if ch in sentence:
+            sentence = sentence.replace(ch,' ')
+
+    # split string on spaces
+    tokens = sentence.split()
+
+    return tokens
+
+#print(tokenize('Hello World.https://funzt.cool/hello/world?q=12332 what the hell?This is great\s.hello . world'))
+
 # 1. Prepare all the tweets, partition into training and test sets; select
 #    about 100 tweets each, which we will be testing on later.
 #    nb: As with any ML task, training and test must not overlap
 
+def get_tokenized_tweets(df: pd.DataFrame, key: str):
+    tweets = df[key].reset_index(drop=True).to_frame()
+    tweets = tweets[key].apply(lambda x: tokenize(x)).to_frame()
+    return tweets
 
+def split_train_test(data, test_size, random_state=42):
+    randomized = data.sample(frac=1, random_state = random_state).reset_index(drop=True)
+    length = data.size
+    #test_size = math.floor(test_size * length)
+    train = randomized.head(length-test_size).reset_index(drop=True)
+    test = randomized.tail(test_size).reset_index(drop=True)
+    return (train, test)
+
+trump_tweets = pd.read_json("../res/tweets_01-08-2021.json")
+trump_tweets = trump_tweets[trump_tweets.isRetweet == 'f']
+trump_tweets = get_tokenized_tweets(trump_tweets, "text")
+display(trump_tweets.head(5))
+trump_train, trump_test = split_train_test(data=trump_tweets, test_size=100)
+
+obama_tweets = pd.read_csv("../res/Tweets-BarackObama.csv")
+obama_tweets = get_tokenized_tweets(obama_tweets, 'Tweet-text')
+display(obama_tweets.head(5))
+obama_train, obama_test = split_train_test(data=obama_tweets, test_size=100)
+
+biden_tweets = pd.read_csv("../res/JoeBidenTweets.csv")
+biden_tweets = get_tokenized_tweets(biden_tweets, 'tweet')
+display(biden_tweets.head(5))
+biden_train, biden_test = split_train_test(data=biden_tweets, test_size=100)
+
+# %%
 # 2. Train n-gram models with n = [1, ..., 5] for both Obama, Trump and Biden.
 # 2.1 Also train a joint model, that will serve as background model
 
+def train_model(data, n: int):
+    model = MLE(n)
+    train, vocab = padded_everygram_pipeline(n, data)
+    model.fit(train, vocab)
+    return model
 
+trained_ngrams = {}
+for n in range(1, 6):
+    print(n)
+    trump_model = train_model(trump_train['text'].tolist(), n)
+    print(f'trump {n} finished')
+    obama_model  = train_model(obama_train['Tweet-text'].tolist(), n)
+    print(f'obama {n} finished')
+    biden_model  = train_model(biden_train['tweet'].tolist(), n)
+    print(f'biden {n} finished')
+    combined_train = []
+    combined_train.extend(trump_train['text'].tolist())
+    combined_train.extend(obama_train['Tweet-text'].tolist())
+    combined_train.extend(biden_train['tweet'].tolist())
+    combined_model = train_model(combined_train, n)
+    print(f'combined {n} finished')
+
+    trained_ngrams[n] = {
+        "trump": trump_model, 
+        "obama": obama_model, 
+        "biden": biden_model, 
+        "combined": combined_model
+    }
+# %%
+
+print(trained_ngrams[3]['trump'].generate(20))
+#print(list(trained_ngrams[3]['trump'].vocab)[:50])
+
+# %%
 # 3. Use the log-ratio method to classify the tweets. Trump should be easy to
 #    spot; but what about Biden vs. Trump?
 # 3.1 Analyze: At what context length (n) does the system perform best?
 
+class MaxSizeList(object):
+
+    def __init__(self, max_length):
+        self.max_length = max_length
+        self.list = []
+
+    def push(self, st):
+        self.list.append(st)
+        if len(self.list) > self.max_length:
+            self.list.pop(0)
+
+    def get_list(self):
+        return self.list
+
+def get_score(text: list, model: MLE):
+    prev_words = MaxSizeList(model.order - 1)
+    score = 1
+    for word in text:
+        # map unknown words to <UNK> token
+        word = model.vocab.lookup(word)
+
+        # calc score
+        tmp_score = model.logscore(word, prev_words.get_list())
+        if(tmp_score != math.inf and tmp_score != -math.inf):
+            score = score * model.logscore(word, prev_words.get_list())
+            
+        #print(f'{word} \t| {prev_words.get_list()} \t= {model.logscore(word, prev_words.get_list())}')
+        prev_words.push(word)
+
+    return abs(score)
+
+#print(trump_test['text'][11])
+#print(get_score(trump_test['text'][11], trained_ngrams[3]['obama']))
+
+
+def predict(text: list, models, keys: list, ngram: int):
+
+    scores = []
+    for key in keys:
+        score = (get_score(text, models[ngram][key]), key)
+        scores.append(score)
+
+    #print(scores)
+    x = scores[0][0] / scores[1][0] if scores[1][0] else 0
+    s = math.log(x) if x !=0 else -math.inf
+    if s > 0:
+        return scores[0][1]
+    else:
+        return scores[1][1]
+    #maximum = max(scores, key=lambda x: x[0])
+    #return maximum[1]
+
+#print(trump_test['text'][13])
+#print(predict(trump_test['text'][13], trained_ngrams, ['trump', 'obama'], 3))
+
+def plot_ConfusionMatrix(df):
+    encoder = LabelEncoder()
+    encoder.fit(df['actual'])
+    act = encoder.transform(df['actual'])
+    pred = encoder.transform(df['predicted'])
+
+    cm = confusion_matrix(act, pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                display_labels=encoder.classes_)
+    disp.plot()
+    plt.show()
+
+print('trump vs. obama')
+for n in range(1,6):
+    print(n)
+    df = pd.DataFrame(columns=['actual', 'predicted'])
+    for tweet in trump_test['text']:
+        df.loc[len(df.index)] = ['trump', predict(tweet, trained_ngrams, ['trump', 'obama'], n)]
+    for tweet in obama_test['Tweet-text']:
+        df.loc[len(df.index)] = ['obama', predict(tweet, trained_ngrams, ['trump', 'obama'], n)]
+
+    plot_ConfusionMatrix(df)
+
+print('trump vs. biden')
+for n in range(1,6):
+    print(n)
+    df = pd.DataFrame(columns=['actual', 'predicted'])
+    for tweet in trump_test['text']:
+        df.loc[len(df.index)] = ['trump', predict(tweet, trained_ngrams, ['trump', 'biden'], n)]
+    for tweet in biden_test['tweet']:
+        df.loc[len(df.index)] = ['biden', predict(tweet, trained_ngrams, ['trump', 'biden'], n)]
+
+    plot_ConfusionMatrix(df)
+
+print('biden vs. obama')
+for n in range(1,6):
+    print(n)
+    df = pd.DataFrame(columns=['actual', 'predicted'])
+    for tweet in biden_test['tweet']:
+        df.loc[len(df.index)] = ['biden', predict(tweet, trained_ngrams, ['biden', 'obama'], n)]
+    for tweet in obama_test['Tweet-text']:
+        df.loc[len(df.index)] = ['obama', predict(tweet, trained_ngrams, ['biden', 'obama'], n)]
+
+    plot_ConfusionMatrix(df)
+
+# %%
 
 # 4. Compute (and plot) the perplexities for each of the test tweets and 
 #    models. Is picking the Model with minimum perplexity a better classifier
 #    than in 3.?
+
+# %%
--- src/theses.py
+++ src/theses.py
@@ -1,16 +1,69 @@
 #!/usr/bin/env python3
 
+# %%
 # we will be using nltk.lm and numpy
+from cgitb import lookup
+from pyexpat import model
+from matplotlib.pyplot import text
+from matplotlib.style import context
 import numpy as np
-import nltk
+from nltk import everygrams
+from nltk.lm import MLE
+from nltk.probability import FreqDist
+from nltk.lm.preprocessing import padded_everygram_pipeline
+import random
 
 
 # 0. Before you get started, make sure to download the `theses.txt` data set.
 
+def get_data() -> list :
+    f = open('../res/theses.txt', 'r')
+    lines = f.readlines()
+    f.close()
+
+    for idx, line in enumerate(lines):
+        lines[idx] = line.replace("\n", "")    
+    return lines
+
 # 1. Spend some time on pre-processing. How would you handle hyphenated words
 #    and abbreviations/acronyms?
 
+def tokenize(data: str) -> list:
+    return data.split()
+
 # 2. Train n-gram models with n = [1, ..., 5]. What about <s> and </s>?
+
+def train_model(data, n: int):
+    model = MLE(n)
+    train, vocab = padded_everygram_pipeline(n, data)
+    model.fit(train, vocab)
+    return model
+
+def get_everygrams(data: list, min_n: int, max_n: int):
+    grams = []
+    for idx, sentence in enumerate(data):
+        gram = everygrams(
+            sentence,
+            min_len=min_n, 
+            max_len=max_n, 
+            pad_left=True, 
+            pad_right=True,
+            left_pad_symbol="<s>",
+            right_pad_symbol="</s>")
+        grams.append(gram)
+
+    return grams
+
+data = get_data()
+tokenized_titles = [tokenize(title) for title in data]
+
+trained_models = {}
+for n in range(1, 6):
+    model = train_model(tokenized_titles, n)
+    print(f'{n}-gram model finished training')
+    trained_models[n] = model
+
+# %%
 
 # 3. Write a generator that provides thesis titles of desired length. Please
 #    do not use the available `lm.generate` method but write your own.
@@ -19,5 +72,96 @@ import nltk
 # 3.1 How can you incorporate seed words?
 # 3.2 How do you handle </s> tokens (w.r.t. the desired length?)
 
+class MaxSizeList(object):
+
+    def __init__(self, max_length):
+        self.max_length = max_length
+        self.list = []
+
+    def push(self, st):
+        self.list.append(st)
+        if len(self.list) > self.max_length:
+            self.list.pop(0)
+
+    def get_list(self):
+        return self.list
+
+def generate_lookup(ngrams: list):
+    fdist = FreqDist()
+
+    for entry in ngrams:
+        fdist.update(list(entry))
+
+    lookup = {}
+    for ngram in fdist:        
+        key =  ngram[:-1]
+        word = ngram[-1]
+        count = fdist[ngram]
+
+        if key not in lookup:
+            lookup[key] = {}
+        
+        lookup[key][word] = count
+
+    return lookup
+
+def generate_thesis(ngrams: list, n: int, text_seed: str = '<s>', length: int = 10):
+    
+    
+    def get_next(context: tuple):
+        values = lookup[context]
+
+        choices = []
+        counts =  []
+
+        for choice in values.keys(): 
+            choices.append(choice)
+            counts.append(values[choice])
+
+        word = random.choices(choices, weights=counts, k=1)
+        return word[0]
+
+    lookup = generate_lookup(ngrams)
+    context = MaxSizeList(n-1)
+    context.push(text_seed)
+
+    title = []
+    counter = 0
+    while counter <= length:
+        w = get_next(tuple(context.get_list()))
+        if w == '<s>':
+            continue
+
+        if w == '</s>':
+            break
+
+        title.append(w)        
+        context.push(w)
+
+    return title
+# %%
+
 # 3.3 If you didn't just copy what nltk's lm.generate does: compare the
 #     outputs
+
+num_words = 10
+
+def remove_items(given_list, item):
+    # using list comprehension to perform the task
+    res = [i for i in given_list if i != item]
+    return res
+
+
+for n in range(2,6):
+    print(f'--------- {n} ---------')
+    generated_tokens = trained_models[n].generate(num_words, text_seed=['<s>'], random_seed=42)
+    generated_tokens = remove_items(generated_tokens, '<s>')
+    generated_tokens = remove_items(generated_tokens, '</s>')
+    
+    print(' '.join(generated_tokens))
+
+    ngrams = get_everygrams(tokenized_titles, 1, n)
+    thesis = generate_thesis(ngrams=ngrams, text_seed='<s>', length=num_words, n=n)
+    print(' '.join(thesis))
+
+# %%
